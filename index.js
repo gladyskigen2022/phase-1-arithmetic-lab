82b9ddc34a615d80f3922f75a9af0b2323945de6
GladysKigen <gladys.kigen@student.moringa.com>
2022-11-24 14:37:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Write your code here
+
+//let x = multiply(31, 2);   // Function is called, return value will end up in x
+
+//function multiply(num1,num2) {
+ // return num1 * num2;             // Function returns the product of a and b
+//}
+//
+    
+//let num1 = 31
+//let num2 = 2
+
+//let multiply = num1 * num2
+
+//function multiply(num1,num2){
+
+   //return num1*num2
+//}
+//console.log(multiply(31,2))
+
+let num1=31;
+let num2=2;
+let multiply = 31*2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3=24;
+let num4=5;
+let mod = num3%num4;
+
+let max = Math.max(1, 20, 3, 4, 5,16,17);
+
+
